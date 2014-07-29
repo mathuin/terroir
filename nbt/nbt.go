7ecf717b90774aada026a3c5eeb90d3b7ba3fdcd
Jack Twilley <mathuin@gmail.com>
2014-07-28 21:58:51 -0700

    Increased error checks.
    Downside is that it's a few more untested lines of code since I'm not
    going down every error branch.  Upside is that errors make it all the
    way up, which I think is good.
    
    Still haven't done list.

--- nbt/nbt.go
+++ nbt/nbt.go
@@ -27,8 +27,8 @@ type Tag struct {
 	Payload interface{}
 }
 
-type PayloadReader func(io.Reader) interface{}
-type PayloadWriter func(io.Writer, interface{})
+type PayloadReader func(io.Reader) (interface{}, error)
+type PayloadWriter func(io.Writer, interface{}) error
 
 var Tags = map[byte]struct {
 	String  string
@@ -40,126 +40,139 @@ var Tags = map[byte]struct {
 		nil,
 	},
 	TAG_Byte: {"TAG_Byte",
-		func(r io.Reader) interface{} {
+		func(r io.Reader) (interface{}, error) {
 			payload := make([]byte, 1)
 			if _, err := io.ReadFull(r, payload); err != nil {
-				panic(err)
+				return nil, err
 			}
-			return payload
+			return payload[0], nil
 		},
-		func(w io.Writer, i interface{}) {
-			w.Write(i.([]byte))
+		func(w io.Writer, i interface{}) error {
+			b := make([]byte, 1)
+			b[0] = i.(byte)
+			_, err := w.Write(b)
+			return err
 		},
 	},
 	TAG_Short: {"TAG_Short",
-		func(r io.Reader) interface{} {
+		func(r io.Reader) (interface{}, error) {
 			var payload int16
 			if err := binary.Read(r, binary.BigEndian, &payload); err != nil {
-				panic(err)
+				return nil, err
 			}
-			return payload
+			return payload, nil
 		},
-		func(w io.Writer, i interface{}) {
-			binary.Write(w, binary.BigEndian, i.(int16))
+		func(w io.Writer, i interface{}) error {
+			return binary.Write(w, binary.BigEndian, i.(int16))
 		},
 	},
 	TAG_Int: {"TAG_Int",
-		func(r io.Reader) interface{} {
+		func(r io.Reader) (interface{}, error) {
 			var payload int32
 			if err := binary.Read(r, binary.BigEndian, &payload); err != nil {
-				panic(err)
+				return nil, err
 			}
-			return payload
+			return payload, nil
 		},
-		func(w io.Writer, i interface{}) {
-			binary.Write(w, binary.BigEndian, i.(int32))
+		func(w io.Writer, i interface{}) error {
+			return binary.Write(w, binary.BigEndian, i.(int32))
 		},
 	},
 	TAG_Long: {"TAG_Long",
-		func(r io.Reader) interface{} {
+		func(r io.Reader) (interface{}, error) {
 			var payload int64
 			if err := binary.Read(r, binary.BigEndian, &payload); err != nil {
-				panic(err)
+				return nil, err
 			}
-			return payload
+			return payload, nil
 		},
-		func(w io.Writer, i interface{}) {
-			binary.Write(w, binary.BigEndian, i.(int64))
+		func(w io.Writer, i interface{}) error {
+			return binary.Write(w, binary.BigEndian, i.(int64))
 		},
 	},
 	TAG_Float: {"TAG_Float",
-		func(r io.Reader) interface{} {
+		func(r io.Reader) (interface{}, error) {
 			var payload float32
 			if err := binary.Read(r, binary.BigEndian, &payload); err != nil {
-				panic(err)
+				return nil, err
 			}
-			return payload
+			return payload, nil
 		},
-		func(w io.Writer, i interface{}) {
-			binary.Write(w, binary.BigEndian, i.(float32))
+		func(w io.Writer, i interface{}) error {
+			return binary.Write(w, binary.BigEndian, i.(float32))
 		},
 	},
 	TAG_Double: {"TAG_Double",
-		func(r io.Reader) interface{} {
+		func(r io.Reader) (interface{}, error) {
 			var payload float64
 			if err := binary.Read(r, binary.BigEndian, &payload); err != nil {
-				panic(err)
+				return nil, err
 			}
-			return payload
+			return payload, nil
 		},
-		func(w io.Writer, i interface{}) {
-			binary.Write(w, binary.BigEndian, i.(float64))
+		func(w io.Writer, i interface{}) error {
+			return binary.Write(w, binary.BigEndian, i.(float64))
 		},
 	},
 	TAG_Byte_Array: {"TAG_Byte_Array",
-		func(r io.Reader) interface{} {
+		func(r io.Reader) (interface{}, error) {
 			var strlen int32
 			if err := binary.Read(r, binary.BigEndian, &strlen); err != nil {
-				panic(err)
+				return nil, err
 			}
 
 			strbytes := make([]byte, strlen)
 
 			if _, err := io.ReadFull(r, strbytes); err != nil {
-				panic(err)
+				return nil, err
 			}
 
-			return strbytes
+			return strbytes, nil
 		},
-		func(w io.Writer, i interface{}) {
-			binary.Write(w, binary.BigEndian, int32(len(i.([]byte))))
-			for _, value := range i.([]byte) {
-				binary.Write(w, binary.BigEndian, value)
+		func(w io.Writer, i interface{}) error {
+			if err := binary.Write(w, binary.BigEndian, int32(len(i.([]byte)))); err != nil {
+				return err
 			}
+			for _, value := range i.([]byte) {
+				if err := binary.Write(w, binary.BigEndian, value); err != nil {
+					return err
+				}
+			}
+			return nil
 		},
 	},
 	TAG_String: {"TAG_String",
-		func(r io.Reader) interface{} {
+		func(r io.Reader) (interface{}, error) {
 			var strlen int16
 			if err := binary.Read(r, binary.BigEndian, &strlen); err != nil {
-				panic(err)
+				return nil, err
 			}
 
 			strbytes := make([]byte, strlen)
 
 			if _, err := io.ReadFull(r, strbytes); err != nil {
-				panic(err)
+				return nil, err
 			}
 
-			return string(strbytes)
+			return string(strbytes), nil
 		},
-		func(w io.Writer, i interface{}) {
-			binary.Write(w, binary.BigEndian, int16(len(i.(string))))
-			w.Write([]byte(i.(string)))
+		func(w io.Writer, i interface{}) error {
+			if err := binary.Write(w, binary.BigEndian, int16(len(i.(string)))); err != nil {
+				return err
+			}
+			_, err := w.Write([]byte(i.(string)))
+			return err
 		},
 	},
 	TAG_List: {"TAG_List",
-		func(r io.Reader) interface{} {
+		func(r io.Reader) (interface{}, error) {
 			var i interface{}
-			return i
+			var err error
+			return i, err
 		},
-		func(w io.Writer, i interface{}) {
-			return
+		func(w io.Writer, i interface{}) error {
+			var err error
+			return err
 		},
 	},
 	TAG_Compound: {"TAG_Compound",
@@ -168,26 +181,31 @@ var Tags = map[byte]struct {
 		nil,
 	},
 	TAG_Int_Array: {"TAG_Int_Array",
-		func(r io.Reader) interface{} {
+		func(r io.Reader) (interface{}, error) {
 			var strlen int32
 			if err := binary.Read(r, binary.BigEndian, &strlen); err != nil {
-				panic(err)
+				return nil, err
 			}
 
 			ints := make([]int32, strlen)
 			for key := range ints {
 				if err := binary.Read(r, binary.BigEndian, &ints[key]); err != nil {
-					panic(err)
+					return nil, err
 				}
 			}
 
-			return ints
+			return ints, nil
 		},
-		func(w io.Writer, i interface{}) {
-			binary.Write(w, binary.BigEndian, int32(len(i.([]int32))))
-			for _, value := range i.([]int32) {
-				binary.Write(w, binary.BigEndian, value)
+		func(w io.Writer, i interface{}) error {
+			if err := binary.Write(w, binary.BigEndian, int32(len(i.([]int32)))); err != nil {
+				return err
 			}
+			for _, value := range i.([]int32) {
+				if err := binary.Write(w, binary.BigEndian, value); err != nil {
+					return err
+				}
+			}
+			return nil
 		},
 	},
 }
@@ -195,47 +213,57 @@ var Tags = map[byte]struct {
 // JMT: The compound reader and writer cause an initialization loop
 // when added to the Tags variable.
 
-func readCompound(r io.Reader) interface{} {
+func readCompound(r io.Reader) (interface{}, error) {
 	payload := []Tag{}
 	var emptytag Tag
 	for newtag, err := ReadTag(r); newtag != emptytag; newtag, err = ReadTag(r) {
 		if err != nil {
-			panic(err)
+			return nil, err
 		}
-
 		payload = append(payload, newtag)
 	}
-	return payload
+	return payload, nil
 }
 
-func writeCompound(w io.Writer, i interface{}) {
+func writeCompound(w io.Writer, i interface{}) error {
 	tags := append(i.([]Tag), Tag{Type: TAG_End})
 	for _, tag := range tags {
-		WriteTag(w, tag)
+		if err := WriteTag(w, tag); err != nil {
+			return err
+		}
 	}
+	return nil
 }
 
 func ReadTag(r io.Reader) (t Tag, err error) {
 	// read a byte
-	ttype := Tags[TAG_Byte].PReader(r).([]byte)[0]
+	var ttypei, tnamei interface{}
+	if ttypei, err = Tags[TAG_Byte].PReader(r); err != nil {
+		return t, err
+	}
+	ttype := ttypei.(byte)
 
 	// TAG_End isn't really a tag
 	if ttype == TAG_End {
 		return
 	}
 
-	// Real tags need types
-	t.Type = ttype
-
 	// Now about that name
-	t.Name = Tags[TAG_String].PReader(r).(string)
+	if tnamei, err = Tags[TAG_String].PReader(r); err != nil {
+		return
+	}
+	tname := tnamei.(string)
 
 	// Putting this in the widget causes an initialization loop issue
 	// (Tags refers to readCompound refers to ReadTag refers to Tags)
-	if t.Type == TAG_Compound {
-		t.Payload = readCompound(r)
-	} else if val, ok := Tags[t.Type]; ok {
-		t.Payload = val.PReader(r)
+	if ttype == TAG_Compound {
+		if payload, err := readCompound(r); err == nil {
+			t = Tag{Type: ttype, Name: tname, Payload: payload}
+		}
+	} else if val, ok := Tags[ttype]; ok {
+		if payload, err := val.PReader(r); err == nil {
+			t = Tag{Type: ttype, Name: tname, Payload: payload}
+		}
 	} else {
 		err = fmt.Errorf("unknown tag")
 	}
@@ -243,8 +271,7 @@ func ReadTag(r io.Reader) (t Tag, err error) {
 }
 
 func WriteTag(w io.Writer, t Tag) (err error) {
-	// JMT: this []byte{} bit feels wrong
-	Tags[TAG_Byte].PWriter(w, []byte{t.Type})
+	Tags[TAG_Byte].PWriter(w, t.Type)
 
 	if t.Type == TAG_End {
 		return
@@ -254,9 +281,9 @@ func WriteTag(w io.Writer, t Tag) (err error) {
 
 	// JMT: initialization loop issue here too
 	if t.Type == TAG_Compound {
-		writeCompound(w, t.Payload)
+		err = writeCompound(w, t.Payload)
 	} else if val, ok := Tags[t.Type]; ok {
-		val.PWriter(w, t.Payload)
+		err = val.PWriter(w, t.Payload)
 	} else {
 		err = fmt.Errorf("unknown tag")
 	}
--- nbt/nbt_test.go
+++ nbt/nbt_test.go
@@ -10,16 +10,16 @@ var tag_tests = []struct {
 	input  Tag
 	output []byte
 }{
-	{Tag{Type: TAG_Byte, Name: "lookbyte", Payload: []byte{'0'}}, []byte{1, 0, 8, 'l', 'o', 'o', 'k', 'b', 'y', 't', 'e', '0'}},
+	{Tag{Type: TAG_Byte, Name: "lookbyte", Payload: byte('0')}, []byte{1, 0, 8, 'l', 'o', 'o', 'k', 'b', 'y', 't', 'e', '0'}},
 	{Tag{Type: TAG_Short, Name: "lookshort", Payload: int16(2)}, []byte{2, 0, 9, 'l', 'o', 'o', 'k', 's', 'h', 'o', 'r', 't', 0, 2}},
 	{Tag{Type: TAG_Int, Name: "lookint", Payload: int32(4)}, []byte{3, 0, 7, 'l', 'o', 'o', 'k', 'i', 'n', 't', 0, 0, 0, 4}},
 	{Tag{Type: TAG_Long, Name: "looklong", Payload: int64(8)}, []byte{4, 0, 8, 'l', 'o', 'o', 'k', 'l', 'o', 'n', 'g', 0, 0, 0, 0, 0, 0, 0, 8}},
 	{Tag{Type: TAG_Float, Name: "lookfloat", Payload: float32(3.14)}, []byte{0x5, 0x0, 0x9, 0x6c, 0x6f, 0x6f, 0x6b, 0x66, 0x6c, 0x6f, 0x61, 0x74, 0x40, 0x48, 0xf5, 0xc3}},
 	{Tag{Type: TAG_Double, Name: "lookdouble", Payload: float64(6.28)}, []byte{0x6, 0x0, 0xa, 0x6c, 0x6f, 0x6f, 0x6b, 0x64, 0x6f, 0x75, 0x62, 0x6c, 0x65, 0x40, 0x19, 0x1e, 0xb8, 0x51, 0xeb, 0x85, 0x1f}},
 	{Tag{Type: TAG_Byte_Array, Name: "lookbytes", Payload: []byte{'1', '2', '3'}}, []byte{0x7, 0x0, 0x9, 0x6c, 0x6f, 0x6f, 0x6b, 0x62, 0x79, 0x74, 0x65, 0x73, 0x0, 0x0, 0x0, 0x3, 0x31, 0x32, 0x33}},
-	{Tag{Type: TAG_String, Name: "lookstring", Payload: "string"}, []byte{8, 0, 10, 'l', 'o', 'o', 'k', 's', 't', 'r', 'i', 'n', 'g', 0, 6, 's', 't', 'r', 'i', 'n', 'g'}},
-	{Tag{Type: TAG_Compound, Name: "", Payload: []Tag{Tag{Type: TAG_Byte, Name: "lookbyte", Payload: []byte{'0'}}, Tag{Type: TAG_Int, Name: "lookint", Payload: int32(4)}}}, []byte{10, 0, 0, 1, 0, 8, 'l', 'o', 'o', 'k', 'b', 'y', 't', 'e', '0', 3, 0, 7, 'l', 'o', 'o', 'k', 'i', 'n', 't', 0, 0, 0, 4, 0}},
-	{Tag{Type: TAG_Compound, Name: "", Payload: []Tag{Tag{Type: TAG_Compound, Name: "lookcompound", Payload: []Tag{Tag{Type: TAG_Byte, Name: "lookbyte", Payload: []byte{'0'}}, Tag{Type: TAG_Int, Name: "lookint", Payload: int32(4)}}}}}, []byte{10, 0, 0, 10, 0, 12, 'l', 'o', 'o', 'k', 'c', 'o', 'm', 'p', 'o', 'u', 'n', 'd', 1, 0, 8, 'l', 'o', 'o', 'k', 'b', 'y', 't', 'e', '0', 3, 0, 7, 'l', 'o', 'o', 'k', 'i', 'n', 't', 0, 0, 0, 4, 0, 0}},
+	{Tag{Type: TAG_String, Name: "lookstring", Payload: string("string")}, []byte{8, 0, 10, 'l', 'o', 'o', 'k', 's', 't', 'r', 'i', 'n', 'g', 0, 6, 's', 't', 'r', 'i', 'n', 'g'}},
+	{Tag{Type: TAG_Compound, Name: "", Payload: []Tag{Tag{Type: TAG_Byte, Name: "lookbyte", Payload: byte('0')}, Tag{Type: TAG_Int, Name: "lookint", Payload: int32(4)}}}, []byte{10, 0, 0, 1, 0, 8, 'l', 'o', 'o', 'k', 'b', 'y', 't', 'e', '0', 3, 0, 7, 'l', 'o', 'o', 'k', 'i', 'n', 't', 0, 0, 0, 4, 0}},
+	{Tag{Type: TAG_Compound, Name: "", Payload: []Tag{Tag{Type: TAG_Compound, Name: "lookcompound", Payload: []Tag{Tag{Type: TAG_Byte, Name: "lookbyte", Payload: byte('0')}, Tag{Type: TAG_Int, Name: "lookint", Payload: int32(4)}}}}}, []byte{10, 0, 0, 10, 0, 12, 'l', 'o', 'o', 'k', 'c', 'o', 'm', 'p', 'o', 'u', 'n', 'd', 1, 0, 8, 'l', 'o', 'o', 'k', 'b', 'y', 't', 'e', '0', 3, 0, 7, 'l', 'o', 'o', 'k', 'i', 'n', 't', 0, 0, 0, 4, 0, 0}},
 	{Tag{Type: TAG_Int_Array, Name: "lookints", Payload: []int32{1, 2, 3}}, []byte{0xb, 0x0, 0x8, 0x6c, 0x6f, 0x6f, 0x6b, 0x69, 0x6e, 0x74, 0x73, 0x0, 0x0, 0x0, 0x3, 0x0, 0x0, 0x0, 0x1, 0x0, 0x0, 0x0, 0x2, 0x0, 0x0, 0x0, 0x3}},
 }
 
