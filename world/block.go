3a11d0b2e54df1060c4d7b462233f60ed024fa7a
Jack Twilley <mathuin@gmail.com>
2014-08-08 12:06:02 -0700

    Started the blocklist and wrote a test for it.
    The test passes.  Woo!

--- world/block.go
+++ world/block.go
@@ -15,7 +15,7 @@ func (b Block) String() string {
 	return fmt.Sprintf("Block{block: %d, data: %d}", b.block, b.data)
 }
 
-func (w *World) Block(pt Point) (*Block, error) {
+func (w World) Block(pt Point) (*Block, error) {
 	s, err := w.Section(pt)
 	if err != nil {
 		return nil, err
@@ -40,4 +40,40 @@ func (w *World) SetBlock(pt Point, b Block) error {
 	WriteNibble(s.Add, i, add)
 	WriteNibble(s.Data, i, byte(b.data))
 	return nil
+}
+
+var BlockNames = map[string]Block{}
+
+func BlockNamed(name string) (*Block, error) {
+	if val, ok := BlockNames[name]; ok {
+		return &val, nil
+	}
+	return nil, fmt.Errorf("block with name %s does not exist!", name)
+}
+
+func (b Block) BlockName() (string, error) {
+	for name := range BlockNames {
+		if BlockNames[name] == b {
+			return name, nil
+		}
+	}
+	return "", fmt.Errorf("block %v has no name!", b)
+}
+
+func init() {
+	for _, bd := range BlockData {
+		b := MakeBlock(bd.Block, bd.Data)
+		for _, name := range bd.Names {
+			BlockNames[name] = b
+		}
+	}
+}
+
+var BlockData = []struct {
+	Block int
+	Data  int
+	Names []string
+}{
+	{0, 0, []string{"Air", "Empty"}},
+	{1, 0, []string{"Stone"}},
 }
--- world/block_test.go
+++ world/block_test.go
@@ -1,1 +1,10 @@
 package world
+
+import "testing"
+
+func Test_checkBlockData(t *testing.T) {
+	b := BlockNames["Stone"]
+	if b.block != 1 || b.data != 0 {
+		t.Fail()
+	}
+}
