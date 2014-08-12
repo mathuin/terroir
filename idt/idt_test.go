04a3debaec56ff7393ad98367cbb62a528dfa47a
Jack Twilley <mathuin@gmail.com>
2014-08-11 19:59:28 -0700

    Added concurrency and debugging to IDT code, plus dot product!
    Works in the trivial case, hopefully works on the more complicated ones!

--- idt/idt_test.go
+++ idt/idt_test.go
@@ -2,26 +2,19 @@ package idt
 
 import "testing"
 
-// tests:
-// I HAVE NO IDEA
-// make it run right
-// put in sample values
-// get sample output
-// write them to test
-
 var IDT_tests = []struct {
 	coords [][2]float64
 	values []int
 	base   [][2]int
-	outf   []int
-	outt   []int
+	outf   []int16
+	outt   []int16
 }{
 	{
 		[][2]float64{{0.0, 0.1}, {0.2, 0.3}, {0.4, 0.5}},
 		[]int{11, 23, 41},
 		[][2]int{{0, 0}, {0, 1}, {1, 0}, {1, 1}},
-		[]int{10, 26, 25, 28},
-		[]int{41, 11, 11, 11},
+		[]int16{12, 28, 27, 30},
+		[]int16{41, 11, 11, 11},
 	},
 }
 
@@ -42,15 +35,36 @@ func Test_IDT(t *testing.T) {
 
 		for i, v := range outf {
 			if tt.outf[i] != v {
-				t.Errorf("idt: given %+#v %+#v %+#v %+#v, expected %+#v, got %+#v ", tt.coords, tt.values, tt.base, tt.outf, outf)
+				t.Errorf("idt: given %+#v %+#v %+#v, expected %+#v, got %+#v ", tt.coords, tt.values, tt.base, tt.outf, outf)
 				break
 			}
 		}
 		for i, v := range outt {
 			if tt.outt[i] != v {
-				t.Errorf("majority: given %+#v %+#v %+#v %+#v, expected %+#v, got %+#v ", tt.coords, tt.values, tt.base, tt.outt, outt)
+				t.Errorf("majority: given %+#v %+#v %+#v, expected %+#v, got %+#v ", tt.coords, tt.values, tt.base, tt.outt, outt)
 				break
 			}
+		}
+	}
+}
+
+var dot_tests = []struct {
+	x   []float64
+	y   []int
+	out int
+}{
+	{[]float64{1.0, 2.0, 3.0}, []int{4, -5, 6}, 12},
+	{[]float64{1, 3, -5}, []int{4, -2, -1}, 3},
+}
+
+func Test_dot(t *testing.T) {
+	for _, tt := range dot_tests {
+		out, err := dot(tt.x, tt.y)
+		if err != nil {
+			t.Fail()
+		}
+		if out != tt.out {
+			t.Errorf("given %+#v %+#v, expected %d, got %d", tt.x, tt.y, tt.out, out)
 		}
 	}
 }
