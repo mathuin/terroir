c92deb6b24f4a904edc5cc0452de0ca085f45722
Jack Twilley <mathuin@gmail.com>
2014-08-11 19:59:28 -0700

    Added concurrency and debugging to IDT code, plus dot product!
    Works in the trivial case, hopefully works on the more complicated ones!

--- idt/idt.go
+++ idt/idt.go
@@ -2,9 +2,14 @@ package idt
 
 import (
 	"fmt"
+	"log"
+	"runtime"
+	"sync"
 
 	"code.google.com/p/biogo.store/kdtree"
 )
+
+var Debug = false
 
 type IDT struct {
 	coordmap map[string]int
@@ -27,15 +32,54 @@ func NewIDT(coords [][2]float64, values []int) (*IDT, error) {
 	return &IDT{coordmap: coordmap, values: values, tree: *tree}, nil
 }
 
-func (idt IDT) Call(base [][2]int, nnear int, majority bool) (outarr []int, err error) {
-	basepts := kdtree.Points{}
-	for _, v := range base {
-		basepts = append(basepts, kdtree.Point{float64(v[0]), float64(v[1])})
+type OrdPt struct {
+	index int
+	pt    kdtree.Point
+}
+
+type OrdVal struct {
+	index int
+	val   int16
+}
+
+func (idt IDT) genOrdPts(base [][2]int, in chan OrdPt) {
+	for i, v := range base {
+		in <- OrdPt{index: i, pt: kdtree.Point{float64(v[0]), float64(v[1])}}
+	}
+	close(in)
+}
+
+func (idt IDT) Call(base [][2]int, nnear int, majority bool) (outarr []int16, err error) {
+	outarr = make([]int16, len(base))
+
+	in := make(chan OrdPt)
+	out := make(chan OrdVal)
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < runtime.NumCPU(); i++ {
+		go func(i int) {
+			wg.Add(1)
+			idt.Reduce(in, out, nnear, majority, i)
+			wg.Done()
+		}(i)
+	}
+	go func() { wg.Wait(); close(out) }()
+	go idt.genOrdPts(base, in)
+
+	for ordval := range out {
+		outarr[ordval.index] = ordval.val
 	}
 
-	outarr = make([]int, len(basepts))
+	return outarr, nil
+}
 
-	for i, q := range basepts {
+func (idt IDT) Reduce(in chan OrdPt, out chan OrdVal, nnear int, majority bool, i int) {
+	for ordpt := range in {
+		ordval := new(OrdVal)
+		ordval.index = ordpt.index
+		q := ordpt.pt
+
 		nk := kdtree.NewNKeeper(nnear)
 		idt.tree.NearestSet(nk, q)
 		distance := []float64{}
@@ -46,6 +90,10 @@ func (idt IDT) Call(base [][2]int, nnear int, majority bool) (outarr []int, err 
 				index = append(index, idt.coordmap[fmt.Sprint(val.Comparable)])
 			}
 		}
+		if Debug {
+			log.Print("distance: ", distance)
+			log.Print("index: ", index)
+		}
 		var wz int
 		if nnear == 1 || distance[0] < 1e-10 {
 			wz = idt.values[index[0]]
@@ -53,15 +101,25 @@ func (idt IDT) Call(base [][2]int, nnear int, majority bool) (outarr []int, err 
 			w := make([]float64, len(distance))
 			var sumw float64
 			for i, d := range distance {
-				w[i] = 1 / d
-				sumw = sumw + w[i]
+				w[i] = 1.0 / d
+				sumw += w[i]
+			}
+			if Debug {
+				log.Print("w: ", w)
+				log.Print("sumw: ", sumw)
 			}
 			for i := range distance {
-				w[i] = w[i] / sumw
+				w[i] /= sumw
+			}
+			if Debug {
+				log.Print("new w: ", w)
 			}
 			values := make([]int, len(index))
 			for i, v := range index {
 				values[i] = idt.values[v]
+			}
+			if Debug {
+				log.Print("values: ", values)
 			}
 			if majority {
 				majordict := make(map[int]float64)
@@ -75,24 +133,25 @@ func (idt IDT) Call(base [][2]int, nnear int, majority bool) (outarr []int, err 
 					}
 				}
 			} else {
+				var err error
 				wz, err = dot(w, values)
 				if err != nil {
 					panic(err)
 				}
 			}
 		}
-		outarr[i] = wz
+		ordval.val = int16(wz)
+		out <- *ordval
 	}
-	return outarr, nil
 }
 
 func dot(x []float64, y []int) (int, error) {
-	var retval int
+	var retval float64
 	if len(x) != len(y) {
 		return 0, fmt.Errorf("lengths do not match")
 	}
 	for i := range x {
-		retval += int(x[i] * float64(y[i]))
+		retval += x[i] * float64(y[i])
 	}
-	return retval, nil
+	return int(retval), nil
 }
