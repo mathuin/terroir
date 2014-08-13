58e5b3357f5dcca2fc837359685020dba41511bb
Jack Twilley <mathuin@gmail.com>
2014-08-12 17:18:11 -0700

    Minor cleanup in utils module.
    Removed unused stuff and pulled unnecessary int() cast.

--- carto/utils.go
+++ carto/utils.go
@@ -5,32 +5,8 @@ import (
 	"os"
 )
 
-type CartoError error
-
 func notnil(err error) bool {
 	return (err != nil && err.Error() != "No Error")
-}
-
-type Float32Arr []float32
-
-func (arr Float32Arr) min() (m float32) {
-	m = arr[0]
-	for _, v := range arr {
-		if v < m {
-			m = v
-		}
-	}
-	return
-}
-
-func (arr Float32Arr) max() (m float32) {
-	m = arr[0]
-	for _, v := range arr {
-		if v > m {
-			m = v
-		}
-	}
-	return
 }
 
 type Float64Arr []float64
@@ -60,7 +36,7 @@ func setIntValue(name string, old int, mymax int, mymin int) int {
 		log.Printf("warning: %s %d outside %d-%d range", name, old, mymin, mymax)
 	}
 
-	return int(min(max(old, mymin), mymax))
+	return min(max(old, mymin), mymax)
 }
 
 func max(a int, b int) int {
