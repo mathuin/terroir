dc70b30972660803092877a71f50d5c766898cc5
Jack Twilley <mathuin@gmail.com>
2014-08-13 13:48:05 -0700

    First attempt at classifying biomes based on other information.
    Ocean, deep ocean, and plains are a good start.

---
+++ carto/biome_test.go
@@ -0,0 +1,68 @@
+package carto
+
+import "testing"
+
+var biome_tests = []struct {
+	x        int
+	y        int
+	maxdepth int
+	lcarr    []int16
+	elevarr  []int16
+	bathyarr []int16
+	outarr   []int16
+}{
+	{
+		5, 7, 2,
+		[]int16{
+			11, 11, 11, 11, 11,
+			11, 11, 11, 11, 11,
+			11, 11, 11, 11, 11,
+			11, 11, 31, 32, 11,
+			11, 11, 11, 11, 11,
+			11, 11, 11, 11, 11,
+			11, 11, 11, 11, 11,
+		},
+		[]int16{
+			62, 62, 62, 62, 62,
+			62, 62, 62, 62, 62,
+			62, 62, 62, 62, 62,
+			62, 62, 63, 63, 62,
+			62, 62, 62, 62, 62,
+			62, 62, 62, 62, 62,
+			62, 62, 62, 62, 62,
+		},
+		[]int16{
+			2, 2, 2, 2, 2,
+			2, 2, 2, 2, 2,
+			2, 1, 1, 1, 1,
+			2, 1, 0, 0, 1,
+			2, 1, 1, 1, 1,
+			2, 2, 2, 2, 2,
+			2, 2, 2, 2, 2,
+		},
+		[]int16{
+			24, 24, 24, 24, 24,
+			24, 24, 24, 24, 24,
+			24, 0, 0, 0, 0,
+			24, 0, 1, 1, 0,
+			24, 0, 0, 0, 0,
+			24, 24, 24, 24, 24,
+			24, 24, 24, 24, 24,
+		},
+	},
+}
+
+func Test_biome(t *testing.T) {
+	for _, tt := range biome_tests {
+		r := MakeRegion("Pie", FloatExtents{-71.575, -71.576, 41.189, 41.191})
+		r.maxdepth = tt.maxdepth
+		outarr := r.biome(tt.lcarr, tt.elevarr, tt.bathyarr)
+
+		for i, v := range outarr {
+			if v != tt.outarr[i] {
+				t.Errorf("expected \n%s, got \n%s", printarr(tt.outarr, tt.x, tt.y), printarr(outarr, tt.x, tt.y))
+				break
+			}
+		}
+	}
+}
