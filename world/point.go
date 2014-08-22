92b455672275f1dcbe2029153976a4341cc337fc
Jack Twilley <mathuin@gmail.com>
2014-08-21 18:09:34 -0700

    Made Points sortable!
    It's sometimes helpful for debugging to sort the points in a list.

--- world/point.go
+++ world/point.go
@@ -29,3 +29,23 @@ func (pt Point) Index() int {
 func (p Point) ChunkXZ() XZ {
 	return XZ{X: floor(p.X, 16), Z: floor(p.Z, 16)}
 }
+
+type Points []Point
+
+func (slice Points) Len() int {
+	return len(slice)
+}
+
+func (slice Points) Less(i, j int) bool {
+	if slice[i].X != slice[j].X {
+		return slice[i].X < slice[j].X
+	}
+	if slice[i].Y != slice[j].Y {
+		return slice[i].Y < slice[j].Y
+	}
+	return slice[i].Z < slice[j].Z
+}
+
+func (slice Points) Swap(i, j int) {
+	slice[i], slice[j] = slice[j], slice[i]
+}
