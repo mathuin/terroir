2a98f40dd63b60a354d06ce492e33b03ff78a328
Jack Twilley <mathuin@gmail.com>
2014-08-23 09:33:48 -0700

    Cleaned up test.
    Moved things around to handle later test examples better.

--- carto/build_test.go
+++ carto/build_test.go
@@ -8,24 +8,26 @@ import (
 )
 
 var buildWorld_tests = []struct {
-	name string
-	ll   FloatExtents
+	name   string
+	ll     FloatExtents
+	elname string
+	lcname string
 }{
-	{"BlockIsland", FloatExtents{-71.575, -71.576, 41.189, 41.191}},
+	{"BlockIsland", FloatExtents{-71.575, -71.576, 41.189, 41.191}, "elevation.tif", "landcover.tif"},
 }
 
 func Test_buildWorld(t *testing.T) {
 	for _, tt := range buildWorld_tests {
 		r := MakeRegion(tt.name, tt.ll)
-		r.vrts["elevation"] = path.Join(tt.name, "elevation.tif")
-		r.vrts["landcover"] = path.Join(tt.name, "landcover.tif")
-		Debug = true
+		r.vrts["elevation"] = path.Join(tt.name, tt.elname)
+		r.vrts["landcover"] = path.Join(tt.name, tt.lcname)
+		// Debug = true
 		r.buildMap()
 		w, err := r.buildWorld()
 		if err != nil {
 			t.Fail()
 		}
-		Debug = false
+		// Debug = false
 
 		w.SetSaveDir("/tmp")
 		w.Write()
